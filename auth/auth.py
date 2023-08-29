18ac69196047713e125cb4c41d047ee09b0e3610
rohan-vishwakarma <rohanvishwakarma685@gmail.com>
2023-08-29 18:29:18 +0530

    Initial commit

---
+++ auth/auth.py
@@ -0,0 +1,37 @@
+
+from flask import request, redirect, url_for, flash, render_template
+
+from db import get_db
+from app import app
+from werkzeug.security import generate_password_hash
+
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
